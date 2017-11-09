6fd58f22472a3430f9a1dc4af1a4a129afd089a6
Jessibe <jessiraga@gmail.com>
2017-11-09 10:32:44 -0600

    subiendo los primeros ejercicios css

--- views/app.js
+++ views/app.js
@@ -20,3 +20,17 @@ ascii utilizoel metodo String.fromCharCode*/
 var codigo = charCodeAt(2)
 console.log (codigo);
 String.fromCharCode(codigo);
+
+/*//El codigo inicia por medio de un prompt, donde
+//el usuario debe ingresar los digitos de su tarjeta.
+var numbersCard = prompt ("Ingrese aquí los 16 digitos de su tarjeta");
+//El número ingresado se convierte a un array por medio de split
+/*var numbersToArray = numbersCard.split ();
+//Los numeros invierten su orden por medio del metodo reverse.
+numbersCard.reverse ();
+/*function (isValidCard);
+{
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
+}
